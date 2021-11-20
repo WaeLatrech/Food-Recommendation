eb914fdcd7ba23814691e1d3b473dbc9584328e3
maatougz <maatoug.medzouhaier@gmail.com>
2021-11-20 13:18:46 +0100

    first test template

--- src/main/java/app/food/recommendation/endpoints/AdminController.java
+++ src/main/java/app/food/recommendation/endpoints/AdminController.java
@@ -1,5 +1,29 @@
 package app.food.recommendation.endpoints;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import app.food.recommendation.services.Services;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Controller
+@Data
+@AllArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	Services service ;
+
+    
+	@GetMapping("/home")
+	public String returnindexadmin() {
+		
+	    return "admin/indexadmin";
+	}
 }
